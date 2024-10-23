f897e66b9f114198b64c08306d719b414e63704c
VaLeNTin-prog94 <valentinforevo@gmail.com>
2024-10-23 22:34:11 +0300

    Первая домашкая

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from task2.views import class_templates,func_templates
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('classes', class_templates),
+    path('func/', func_templates)
 ]
